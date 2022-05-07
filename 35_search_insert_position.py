96f333df47d46a56668dd3d4168fe3fee12c7c4e
michael-lesirge <100492377+michael-lesirge@users.noreply.github.com>
2022-05-06 22:20:22 -0700

    Create 35_search_insert_position.py

---
+++ 35_search_insert_position.py
@@ -0,0 +1,23 @@
+# https://leetcode.com/problems/search-insert-position/
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        """
+        some version of binary seach
+        if it is not found return as close as possible probbly left
+        """
+        
+        left = 0
+        right = len(nums)-1
+        
+        while left <= right:
+            mid = (left + right) // 2
+            mid_num = nums[mid]
+            
+            if mid_num > target:
+                right = mid-1
+            elif mid < target:
+                left = mid+`
+            else:
+                return mid
+        
+        return left
