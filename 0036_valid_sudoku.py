2ae30012fa092e850edfa2e3394be2475005f0fd
michael-lesirge <100492377+michael-lesirge@users.noreply.github.com>
2022-07-11 21:38:33 -0700

    Create 0036_valid_sudoku.py

---
+++ 0036_valid_sudoku.py
@@ -0,0 +1,43 @@
+# https://leetcode.com/problems/valid-sudoku/
+
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        """
+        create funtion to check list of lengnth 9 for duplicate numbers
+        
+        check rows
+        check culumns
+        check boxes
+            create new list for box | list slicing
+        """
+        
+        if IsInvalidRows(board):
+            return False
+        
+        for culumn in range(9):
+            if isInvalidSection([board[row][culumn] for row in range(9)]):
+                return False
+        
+        
+        for row_start in range(0, 9, 3):
+            boxes = [[], [], []]
+            for row in range(row_start, row_start+3):
+                for column, box in zip(range(0, 9, 3), boxes):
+                    box.extend(board[row][column:column+3])
+            if IsInvalidRows(boxes):
+                return False
+            
+        return True
+    
+def IsInvalidRows(arrays) -> bool:
+    return any(map(isInvalidSection, arrays))
+
+def isInvalidSection(array: list) -> bool:
+    seen = set()
+    for item in array:
+        if item == ".":
+            continue
+        if item in seen:
+            return True
+        seen.add(item)
+    return False
