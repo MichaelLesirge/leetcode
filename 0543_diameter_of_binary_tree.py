1c7a9af33f1c50004bbb7cc108f8f3a2cb11eba8
michael-lesirge <100492377+michael-lesirge@users.noreply.github.com>
2022-07-20 14:07:53 -0700

    49 ms, faster than 89.26%
    Runtime: 49 ms, faster than 89.26% of Python3 online submissions for Diameter of Binary Tree.
    Memory Usage: 16.3 MB, less than 42.54% of Python3 online submissions for Diameter of Binary Tree.

---
+++ 0543_diameter_of_binary_tree.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+# https://leetcode.com/problems/diameter-of-binary-tree/submissions/
+class Solution:
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        self.max_diameter = 0
+    
+        def diameterOfNode(root: Optional[TreeNode]) -> int:
+            if root is None:
+                return 0
+            left, right = diameterOfNode(root.left), diameterOfNode(root.right)
+            # self.max_diameter = max(self.max_diameter, left+right)
+            lr_sum = left+right
+            if lr_sum > self.max_diameter:
+                self.max_diameter = lr_sum
+            return max(left, right) + 1
+        
+        
+        diameterOfNode(root)
+        return self.max_diameter
