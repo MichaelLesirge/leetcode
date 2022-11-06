544b544503d01e7f02e09efd09b085e81ac84dca
michael-lesirge <100492377+michael-lesirge@users.noreply.github.com>
2022-11-06 11:01:24 -0800

    remove extra newline and space

--- filename_creator.py
+++ filename_creator.py
@@ -11,63 +11,60 @@ r = Tk()
 r.withdraw()
 
 def display(s: str) -> None:
-	print(s)
-	r.clipboard_clear()
-	r.clipboard_append(s)
-	print()
+    print(s)
+    r.clipboard_clear()
+    r.clipboard_append(s)
+    print()
 
 def ask(prompt: str) -> str:
-	v = input(prompt + ": ")
-	if v.lower() == "exit":
-		raise KeyboardInterrupt
-	return v
+    v = input(prompt + ": ")
+    if v.lower() == "exit":
+        raise KeyboardInterrupt
+    return v
 
 def multi_input_input(prompt: str, lines: int) -> list[str]:
-	print(prompt + ":")
-	return [input() for i in range(lines)]
+    print(prompt + ":")
+    return [input() for i in range(lines)]
 
 def format_title(title: str) -> tuple[str, str]:
-	number, q_name = title.split(".")
-	q_name = q_name.strip()
-	number = number.strip()
-	number = int(number)
-	name = q_name.lower().replace(" ", "_")
-	return f"{number:04d}_{name}.py", q_name
+    number, q_name = title.split(".")
+    q_name = q_name.strip()
+    number = number.strip()
+    number = int(number)
+    name = q_name.lower().replace(" ", "_")
+    return f"{number:04d}_{name}.py", q_name
 
 def format_desc(stats: list[str], question_name: str) -> tuple[str, str]:
-	preformance = f"{stats[1]}, faster than {stats[3]}"
-	memory = f"{stats[5]}, less than {stats[7]}"
+    preformance = f"{stats[1]}, faster than {stats[3]}"
+    memory = f"{stats[5]}, less than {stats[7]}"
 
-	return preformance, f"""
-Runtime: {preformance} of Python3 online submissions for {question_name}.
-Memory Usage: {memory} of Python3 online submissions for {question_name}.
-"""
+    return preformance, f"""Runtime: {preformance} of Python3 online submissions for {question_name}.\nMemory Usage: {memory} of Python3 online submissions for {question_name}."""
 
 def main():
-	print("1. URL at top of code as comment")
-	print("2. Preformance as commit title")
-	print("3. All info as commit description")
-	print()
+    print("1. URL at top of code as comment")
+    print("2. Preformance as commit title")
+    print("3. All info as commit description")
+    print()
 
-	try:
-		while True:
-			title = ask("Enter question title")
-			filename, name = format_title(title)
-			display(filename)
+    try:
+        while True:
+            title = ask("Enter question title")
+            filename, name = format_title(title)
+            display(filename)
 
-			stats = multi_input_input("Enters stats", 8)
-			commit_name, commit_desc = format_desc(stats, question_name=name)
-			display(commit_name)
-			ask("press enter")
-			display(commit_desc)
+            stats = multi_input_input("Enters stats", 8)
+            commit_name, commit_desc = format_desc(stats, question_name=name)
+            display(commit_name)
+            ask("press enter")
+            display(commit_desc)
 
 
-	except KeyboardInterrupt:
-		print()
-		print("Bye bye!")
+    except KeyboardInterrupt:
+        print()
+        print("Bye bye!")
 
-		r.destroy()
-		exit()
+        r.destroy()
+        exit()
 
 if __name__ == "__main__":
-	main()
+    main()
