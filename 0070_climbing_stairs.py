055962e537f4ca6cfb4f135146c82618afe3db0f
michael-lesirge <100492377+michael-lesirge@users.noreply.github.com>
2022-11-06 13:23:38 -0800

    63 ms, faster than 19.31%
    Runtime: 63 ms, faster than 19.31% of Python3 online submissions for Climbing Stairs.
    Memory Usage: 13.9 MB, less than 57.42% of Python3 online submissions for Climbing Stairs.

--- 0070_climbing_stairs.py
+++ 0070_climbing_stairs.py
@@ -1,9 +1,6 @@
 # https://leetcode.com/problems/climbing-stairs/
-
 class Solution:
-    # recursive + memo
-    def climbStairsRecursive(self, n: int, memo={1: 1, 2: 2}) -> int:  # memo = {i: i for i in range(0, 4)}
+    def climbStairs(self, n: int, memo={1: 1, 2: 2}) -> int:
         if n not in memo:
-            memo[n] = self.climbStairsRecursive(n-1) + self.climbStairsRecursive(n-2)
+            memo[n] = self.climbStairs(n-1) + self.climbStairs(n-2)
         return memo[n]
-        
